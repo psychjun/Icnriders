d0ddb95b8eec394353d0dab129342d057e32986a
psychjun <psychjun@gmail.com>
2026-04-20 09:48:47 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
-import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body style={{ margin: 0, backgroundColor: '#0f172a' }}>{children}</body>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body style={{ margin: 0, backgroundColor: '#0f172a' }}>
+        {children}
+      </body>
     </html>
   )
 }
